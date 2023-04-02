5f01f40352ecd33b38ee9716499569cee487369a
Hardik <hardik.nandwani@classplus.co>
2023-04-02 22:23:31 +0530

    edge case for less seats

--- src/routers/train.js
+++ src/routers/train.js
@@ -22,94 +22,6 @@ trainRouter.post("/bookSeat", async (req, res) => {
   let { count } = req.body;
 
   count = parseInt(count);
-
-
-  const seatsInRow = 7; // number of seats in a row
-  const rowsInCoach = 12; // number of rows in the coach
-  const seatsInLastRow = 3; // number of seats in the last row
-
-  //   const bookSeats = async function (numSeats) {
-  //     // Query the database for the number of available seats
-  //     const availableSeatsCount = await Seat.countDocuments({
-  //       status: "available",
-  //     });
-
-  //     // If there are not enough available seats, return an error message
-  //     if (availableSeatsCount < numSeats) {
-  //       return "Sorry, there are not enough seats available.";
-  //     }
-
-  //     // Find contiguous seats in a row
-  //     let seatsToBook = [];
-  //     for (let row = 1; row <= rowsInCoach; row++) {
-  //       let seats = await Seat.find({ row: row, status: "available" })
-
-  //       let candidateSeats = [];
-  //       for (let i = 0; i < seats.length; i++) {
-  //         if (
-  //           seats[i].seat % seatsInRow !== 0 &&
-  //           (seats[i].seat + 1) % seatsInRow !== 1
-  //         ) {
-  //           candidateSeats.push(seats[i]);
-  //         } else {
-  //           candidateSeats = [];
-  //         }
-  //         if (candidateSeats.length === numSeats) {
-  //           seatsToBook = candidateSeats;
-  //           break;
-  //         }
-  //       }
-  //       if (seatsToBook.length > 0) {
-  //         break;
-  //       }
-  //     }
-
-  //     // If contiguous seats are not found, book nearby seats
-  //     if (seatsToBook.length === 0) {
-  //       for (let row = 1; row <= rowsInCoach; row++) {
-  //         let seats = await Seat
-  //           .find({ row: row, status: "available" })
-  //         for (let i = 0; i < seats.length - numSeats + 1; i++) {
-  //           if (i + numSeats > seats.length) {
-  //             break;
-  //           }
-  //           seatsToBook = seats.slice(i, i + numSeats);
-  //           break;
-  //         }
-  //         if (seatsToBook.length > 0) {
-  //           break;
-  //         }
-  //       }
-  //     }
-
-  //     // Update the seats in the database
-  //     let seatIdsToBook = seatsToBook.map((seat) => seat._id);
-  // await Seat.updateMany(
-  //   { _id: { $in: seatIdsToBook } },
-  //   { $set: { status: "reserved" } }
-  // );
-
-  // // get seats booked row and seat number
-  // let seatsBooked = await Seat.find({ _id: { $in: seatIdsToBook } });
-
-  // let response = seatsBooked.map((seat) => {
-  //   return  seat.row +  seat.seat;
-  // });
-
-  // return "Seats booked: " + JSON.stringify(response);
-
-  // let seats = await Seat.aggregate([
-  //   { $match: { status: "available" } },
-  //   { $group: { _id: "$row", seats: { $push: "$seat" }, count: { $sum: 1 } } },
-  //   { $sort: { _id: 1 } },
-  // ]);
-
-  //   };
-
-  // Test the bookSeats function
-  // let result = await bookSeats(count);
-  // res.send(result);
-
   // get seats from db and grop by row order by row and put count of available seats get seat id also
   const seats = await Seat.aggregate([
     { $match: { status: "available" } },
@@ -160,26 +72,30 @@ trainRouter.post("/bookSeat", async (req, res) => {
         while (newSeats.length !== count) {
           newSeats.pop();
         }
-      } 
+      }
       if (newSeats.length === count) {
         seatsToBook = newSeats;
         break;
       }
     }
   }
-  let seatIdsToBook = seatsToBook.map((seat) => seat);
-  await Seat.updateMany(
-    { _id: { $in: seatIdsToBook } },
-    { $set: { status: "reserved" } }
-  );
+  if (seatsToBook?.length) {
+    let seatIdsToBook = seatsToBook.map((seat) => seat);
+    await Seat.updateMany(
+      { _id: { $in: seatIdsToBook } },
+      { $set: { status: "reserved" } }
+    );
 
-  // get seats booked row and seat number
-  let seatsBooked = await Seat.find({ _id: { $in: seatIdsToBook } });
+    // get seats booked row and seat number
+    let seatsBooked = await Seat.find({ _id: { $in: seatIdsToBook } });
 
-  let response = seatsBooked.map((seat) => {
-    return seat.row + seat.seat;
-  });
-  res.send("Seats booked: " + JSON.stringify(response));
+    let response = seatsBooked.map((seat) => {
+      return seat.row + seat.seat;
+    });
+    res.send("Seats booked: " + JSON.stringify(response));
+  } else {
+    res.send("not enough seats to book");
+  }
 });
 
 trainRouter.get("/resetSeats", async (req, res) => {
