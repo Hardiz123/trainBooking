6f550d73db6f42e9bfe6ba8298c449cfa0e6f6f6
Hardik Nandwani <46419306+Hardiz123@users.noreply.github.com>
2023-04-03 12:22:55 +0530

    Update index.js
    updated mongodb uri

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 const url = process.env.DB_URL || "mongodb://localhost:27017/train";
-const connectionUrl = "mongodb+srv://admin-hardiz6996:hardik123@cluster0.lac9u.mongodb.net/?retryWrites=true&w=majority";
+const connectionUrl = url;
 
 const connectToDatabase = async () => {
   try {
